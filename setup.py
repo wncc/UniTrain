f4a63d20b07bb89d32e327e67aac9e2ea61cbfe6
Ishan Upadhyay <ishan121028@gmail.com>
2023-10-08 00:44:57 +0530

    Possibly rectified the error related to no subumodules showing up when shipping to PyPi.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='UniTrain',
-    version='0.2.1',
+    version='0.2.2',
     author='Ishan Upadhyay',
     author_email='ishan.upadhyay.iitb@gmail.com',
     description='A generalized training framework for Deep Learning Tasks',
