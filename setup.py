e1b4065569c3f6b3de66524fa7cd28b071415538
ishan121028 <96590593+ishan121028@users.noreply.github.com>
2023-10-07 12:58:44 +0530

    Remove long_description

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name='UniTrain',
     version='0.1',
     author='Ishan Upadhyay',
     author_email='ishan.upadhyay.iitb@gmail.com',
     description='A generalized training framework for Deep Learning Tasks',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
     url='https://github.com/ishan121028/UniTrain',
     download_url='https://github.com/ishan121028/UniTrain/archive/v0.1.tar.gz',
     packages=['UniTrain'],
